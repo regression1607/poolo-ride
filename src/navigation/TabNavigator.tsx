e014daccd4e8e804737d195b34a6ff371b895a00
Ekansh Kumar <ekumar@ekumar-mn2770.linkedin.biz>
2025-08-25 01:37:03 +0530

    resolve the layout issue

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { colors } from '../theme/colors';
